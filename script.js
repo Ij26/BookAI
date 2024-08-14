6acee3fde089dd828cc9511e5ac708f62f1232d0
Ij26 <114659283+Ij26@users.noreply.github.com>
2024-08-14 12:06:40 +0530

    add files

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
   const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
 
-  if (themeToggle) {
-    themeToggle.addEventListener("click", () => {
-      document.body.classList.toggle("bg-white");
-      document.body.classList.toggle("text-black");
-      document.body.classList.toggle("bg-dark-900");
-      document.body.classList.toggle("text-white");
-    });
+  // Check local storage for theme preference
+  if (localStorage.getItem("theme") === "light") {
+    body.classList.remove("bg-dark-900", "text-white");
+    body.classList.add("bg-white", "text-black");
+    themeToggle.textContent = "Dark Mode";
   }
+
+  themeToggle.addEventListener("click", () => {
+    if (body.classList.contains("bg-dark-900")) {
+      body.classList.remove("bg-dark-900", "text-white");
+      body.classList.add("bg-white", "text-black");
+      themeToggle.textContent = "Dark Mode";
+      localStorage.setItem("theme", "light");
+    } else {
+      body.classList.remove("bg-white", "text-black");
+      body.classList.add("bg-dark-900", "text-white");
+      themeToggle.textContent = "Light Mode";
+      localStorage.setItem("theme", "dark");
+    }
+  });
 });
